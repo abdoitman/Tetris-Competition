53484b499d30ebe43bc3aefc664c747ae056a934
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 13:03:26 +0200

    removed draw_grid

--- tetris.py
+++ tetris.py
@@ -5,14 +5,8 @@ class Tetris:
     def __init__(self, app) -> None:
         self.app = app
 
-    def draw_grid(self):
-        for x in range(10):
-            for y in range(20):
-                pg.draw.rect(self.app.screen, 'black',
-                             (x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
-
     def update(self):
         pass
 
     def draw(self):
-        self.draw_grid()
+        pass
