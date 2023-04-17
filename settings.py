17ad4322e00d287ed501ce55209103324627b88d
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 20:29:45 +0200

    added FIELD_W and FIELD_H

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ FPS = 60
 FIELD_COLOR = (47,48,51)
 
 TILE_SIZE = 40
+FIELD_SIZE = FIELD_W , FIELD_H = 10, 20
 FIELD_RES = 10 * TILE_SIZE , 20 * TILE_SIZE     #standard tetris window is 10 blocks in width, 20 blocks in hieght
 
 INIT_POS_OFFSET = vec(5, 0)
