7d4af4d61983355adec8e33756ab236db320f24f
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 20:40:22 +0200

    improved colision system to see if there's a tetromino below

--- tetris.py
+++ tetris.py
@@ -6,11 +6,29 @@ class Tetris:
     def __init__(self, app) -> None:
         self.app = app
         self.sprite_group = pg.sprite.Group()
+        self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
+
+    def put_tetromino_in_field_array(self):
+        for block in self.tetromino.blocks:
+            x, y = int(block.pos.x), int(block.pos.y)
+            self.field_array[y][x] = block
+
+    def get_field_array(self):
+        return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+
+    def check_if_tetromino_has_landed(self):
+        if self.tetromino.landed:
+            self.put_tetromino_in_field_array()
+            self.tetromino = Tetromino(self)
+
+    def control(self):
+        pass
 
     def update(self):
         if self.app.anim_trigger:
             self.tetromino.update()
+            self.check_if_tetromino_has_landed()
         self.sprite_group.update()
 
     def draw(self):
--- tetromino.py
+++ tetromino.py
@@ -20,7 +20,7 @@ class Block(pg.sprite.Sprite):
     
     def is_colided(self, pos):
         x, y = int(pos.x) , int(pos.y)
-        if 0 <= x < FIELD_W and y < FIELD_H:
+        if 0 <= x < FIELD_W and y < FIELD_H and (y < 0 or not self.tetromino.tetris.field_array[y][x]):
             return False
         return True
 
@@ -30,6 +30,7 @@ class Tetromino:
         self.shape = random.choice(list(TETROMINOS_SHAPE.keys()))
         self.color = TETROMINOS_COLOR[self.shape]
         self.blocks = [Block(self, pos, self.color) for pos in TETROMINOS_SHAPE[self.shape]]
+        self.landed = False
 
     def is_colided(self, blocks_positions):
         return any(map(Block.is_colided, self.blocks, blocks_positions))   
@@ -43,6 +44,8 @@ class Tetromino:
         if not is_colided:
             for block in self.blocks:
                 block.pos += moving_direction
+        elif direction == 'down':
+            self.landed = True
 
     def update(self):
         self.move(direction='down')
