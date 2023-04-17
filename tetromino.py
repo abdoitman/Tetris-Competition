c972d57d958bf8f6b12dbcda1fb1c6f1342ee734
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 13:41:10 +0200

    defined block shape and color

--- tetromino.py
+++ tetromino.py
@@ -6,7 +6,7 @@ class Block(pg.sprite.Sprite):
         self.tetromino = tetromino
 
         super().__init__(tetromino.tetris.sprite_group)
-        self.image = pg.surface([TILE_SIZE, TILE_SIZE])
+        self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill(color)
         
         self.rect = self.image.get_rect()
@@ -14,7 +14,9 @@ class Block(pg.sprite.Sprite):
 
 class Tetromino:
     def __init__(self, tetris) -> None:
-        pass    
+        self.tetris = tetris
+        self.shape = 'T'
+        self.blocks = [Block(self, pos, color) for pos, color in zip(TETROMINOS_SHAPE[self.shape], TETROMINOS_COLOR[self.shape])] 
         
     def update(self):
         pass
