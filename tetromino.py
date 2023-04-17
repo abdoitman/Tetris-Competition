3c5224677da4eda93f32547c052e434d96bf995e
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 20:29:58 +0200

    added colisions

--- tetromino.py
+++ tetromino.py
@@ -17,6 +17,12 @@ class Block(pg.sprite.Sprite):
 
     def update(self):
         self.update_rect_pos()
+    
+    def is_colided(self, pos):
+        x, y = int(pos.x) , int(pos.y)
+        if 0 <= x < FIELD_W and y < FIELD_H:
+            return False
+        return True
 
 class Tetromino:
     def __init__(self, tetris) -> None:
@@ -24,11 +30,19 @@ class Tetromino:
         self.shape = random.choice(list(TETROMINOS_SHAPE.keys()))
         self.color = TETROMINOS_COLOR[self.shape]
         self.blocks = [Block(self, pos, self.color) for pos in TETROMINOS_SHAPE[self.shape]]
-        
+
+    def is_colided(self, blocks_positions):
+        return any(map(Block.is_colided, self.blocks, blocks_positions))   
+
     def move(self, direction):
         moving_direction = MOVING_DIRECTIONS[direction]
-        for block in self.blocks:
-            block.pos += moving_direction
+
+        new_positions = [block.pos + moving_direction for block in self.blocks]
+        is_colided = self.is_colided(new_positions)
+        
+        if not is_colided:
+            for block in self.blocks:
+                block.pos += moving_direction
 
     def update(self):
         self.move(direction='down')
