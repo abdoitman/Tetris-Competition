06b542b0db540e3ea535ce561dfbbe74d5a97020
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 13:03:46 +0200

    added draw_grid to the draw method

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ class App:
         self.clock.tick(FPS)
         self.tetris.update()
     
+    def draw_grid(self):
+        for x in range(10):
+            for y in range(20):
+                pg.draw.rect(self.screen, (40,40,40),
+                             (x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
     def draw(self):
         self.screen.fill(color=FIELD_COLOR)
+        self.draw_grid()
         pg.display.flip()
         self.tetris.draw()
-
+        
     def check_for_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
