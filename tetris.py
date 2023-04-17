7ee3ec89de72e29034456285319cfda9c149142e
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-17 13:21:26 +0200

    added tetromino class

--- tetris.py
+++ tetris.py
@@ -1,12 +1,16 @@
 from settings import *
+from tetromino import Tetromino
 import math
 
 class Tetris:
     def __init__(self, app) -> None:
         self.app = app
+        self.sprite_group = pg.sprite.Group()
+        self.tetromino = Tetromino(self)
 
     def update(self):
-        pass
+        self.tetromino.update()
+        self.sprite_group.update()
 
     def draw(self):
-        pass
+        self.sprite_group.draw(self.app.screen)
