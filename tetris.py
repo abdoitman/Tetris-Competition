63f51a1efc0836da464cf6e9ddeb06734f47658b
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-18 06:21:02 +0200

    added a method to calculate score

--- tetris.py
+++ tetris.py
@@ -1,6 +1,23 @@
 from settings import *
 from tetromino import Tetromino
-import math
+import pygame.freetype as ft
+
+class Text:
+    def __init__(self, app):
+        self.app = app
+        self.font = ft.Font(FONT_PATH)
+
+    def draw(self):
+        self.font.render_to(self.app.screen, (WIN_W * 0.685, WIN_H * 0.15),
+                            text= "Next", fgcolor= 'white', size= TILE_SIZE * 1.6)
+        self.font.render_to(self.app.screen, (WIN_W * 0.6667, WIN_H * 0.45),
+                            text= "Level", fgcolor= 'white', size= TILE_SIZE * 1.6)
+        self.font.render_to(self.app.screen, (WIN_W * 0.6667, WIN_H * 0.55),
+                            text= "Level", fgcolor= 'white', size= TILE_SIZE * 1.4)
+        self.font.render_to(self.app.screen, (WIN_W * 0.685, WIN_H * 0.7),
+                            text= "Score", fgcolor= 'white', size= TILE_SIZE * 1.6)
+        self.font.render_to(self.app.screen, (WIN_W * 0.685, WIN_H * 0.8),
+                            text= f"{self.app.tetris.score}", fgcolor= 'white', size= TILE_SIZE * 1.4)
 
 class Tetris:
     def __init__(self, app) -> None:
@@ -10,6 +27,22 @@ class Tetris:
         self.tetromino = Tetromino(self)
         self.next_tetromino = Tetromino(self, current= False)            
         self.speed_up = False
+
+        self.level = 1
+        self.score = 0
+        self.full_lines = 0
+        self.total_lines_cleared = 0
+        self.points_per_line = {
+            0 : 0,
+            1 : 800,
+            2 : 1200,
+            3 : 1800,
+            4 : 2000
+        }
+
+    def get_score(self):
+        self.score += self.points_per_line[self.full_lines]
+        self.full_lines = 0
 
     def put_tetromino_in_field_array(self):
         for block in self.tetromino.blocks:
@@ -40,6 +73,9 @@ class Tetris:
                     self.field_array[row][x].alive = False
                     self.field_array[row][x] = 0
 
+                self.full_lines += 1
+                self.total_lines_cleared += 1
+
     def check_if_tetromino_has_landed(self):
         if self.tetromino.landed:
             if self.is_game_over():
@@ -68,6 +104,7 @@ class Tetris:
             self.check_full_lines()
             self.tetromino.update()
             self.check_if_tetromino_has_landed()
+            self.get_score()
         self.sprite_group.update()
 
     def draw(self):
