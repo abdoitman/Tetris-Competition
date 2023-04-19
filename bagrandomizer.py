1c718bce23e07697c9c3077a2d5d9e4ae9087fa0
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-19 03:29:42 +0200

    implemented the 7 bag randomizer

---
+++ bagrandomizer.py
@@ -0,0 +1,31 @@
+import random
+
+bag = ['S', 'L', 'Z', 'O', 'T', 'I', 'J']
+
+def get_next_shape():
+    global bag
+    while True:
+        if bag == []:
+            bag = ['S', 'L', 'Z', 'O', 'T', 'I', 'J']
+            random.shuffle(bag)
+            yield bag.pop()
+        else:
+            random.shuffle(bag)
+            yield bag.pop()
+
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
+print(next(get_next_shape()))
