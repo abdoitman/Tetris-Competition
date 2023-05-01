8001d73fe6b261cf6eb5fd77c3f452733755e66f
Abdulrahman Itman <abdoitman@gmail.com>
2023-05-01 20:21:47 +0300

    implemented local solver

--- local_submission.py
+++ local_submission.py
@@ -1,5 +1,14 @@
 from game import *
 
-def local_solver(logical_map, current_tetromino, next_tetromino, time_left, level, score, lines_cleared):
-    pass
+def local_solver(logical_map, current_tetromino, next_tetromino, time_left, level, score, lines_cleared) -> list:
 
+    if current_tetromino == 'S':
+        return ['MOV_L', 'MOV_L', 'MOV_L', 'MOV_L', "MOV_D"]
+    else:
+        return ['MOV_R', 'MOV_R', 'MOV_R', 'MOV_R']
+
+
+if __name__ == "__main__":
+    new_game = App()
+    new_game.tetris.solver = local_solver
+    new_game.run()
