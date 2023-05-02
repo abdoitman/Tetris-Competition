c1721d14c90b3a937161e3a3bf8b1f92d9d876b5
Abdulrahman Itman <abdoitman@gmail.com>
2023-05-02 06:00:06 +0300

    teams can write the algorithm in local_solver function

--- local_submission.py
+++ local_submission.py
@@ -1,12 +1,7 @@
 from game import *
 
 def local_solver(logical_map, current_tetromino, next_tetromino, time_left, level, score, lines_cleared) -> list:
-
-    if current_tetromino == 'S':
-        return ['MOV_L', 'MOV_L', 'MOV_L', 'MOV_L', "MOV_D"]
-    else:
-        return ['MOV_R', 'MOV_R', 'MOV_R', 'MOV_R']
-
+    pass
 
 if __name__ == "__main__":
     new_game = App()
