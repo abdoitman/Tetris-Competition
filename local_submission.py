6ee050b54ad79731420b02391e8721c0346812ad
Abdulrahman Itman <abdoitman@gmail.com>
2023-05-07 00:34:54 +0300

    changed return value

--- local_submission.py
+++ local_submission.py
@@ -1,7 +1,7 @@
 from game import *
 
 def local_solver(logical_map, current_tetromino, next_tetromino, time_left, level, score, lines_cleared) -> list:
-    pass
+    return []
 
 if __name__ == "__main__":
     new_game = App()
