576675022b598ab605e6780e35763423319500a9
Abdulrahman Itman <abdoitman@gmail.com>
2023-05-06 06:49:22 +0300

    added documentation to the file

--- server_submission.py
+++ server_submission.py
@@ -1,23 +1,26 @@
 #########################################
 ## WELCOME TO THE CONTEST, HAVE FUN !! ##
 #########################################
-### ENTER THE REQUIRED INFO ABOUT THE ###
-### TEAM TO GET STARTED WITH THE GAME ###
-## REMEMBER THAT YOU WILL GET ONLY TEN ##
-### SUBMISSIONS TO THE SERVER BUT YOU ###
+### ENTER THE REQUIRED INFO ABOUT THE TEAM TO GET STARTED WITH THE GAME ###
+## REMEMBER THAT YOU WILL GET ONLY 10 SUBMISSIONS TO THE SERVER BUT YOU ###
 ## CAN TRY LOCALLY AS MUCH AS YOU WANT ##
 #########################################
 
 ##########################
 # Enter the ID (consisting of 8 characters)
 # that was given to your team leader.
+
 TEAM_ID = ""
+NAME = ""
 ##########################
 
 from game import App
 
+def server_solver(logical_map, current_tetromino, next_tetromino, time_left, level, score, lines_cleared) -> list:
+    pass
 
 
 if __name__ == "__main__":
-    game = App(server= True)
-    game.run()
+    server_game = App(server= True, Team_id= TEAM_ID)
+    server_game.tetris.solver = server_solver
+    server_game.run()
