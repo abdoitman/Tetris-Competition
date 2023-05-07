a39609e7edfd765e59b9ac236606bd793c70f27a
Abdulrahman Itman <abdoitman@gmail.com>
2023-05-07 14:24:31 +0300

    added game files to game package

--- local_submission.py
+++ local_submission.py
@@ -1,4 +1,4 @@
-from game_files.game import *
+from game.game import *
 
 def local_solver(logical_map, current_tetromino, next_tetromino, time_left, level, score, lines_cleared) -> list:
     return []
